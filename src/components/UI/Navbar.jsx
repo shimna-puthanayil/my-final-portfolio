64ab93849a202a3bd175dd156e50c8f0a2965a71
shimna <chimmu.p@gmail.com>
2023-10-04 17:18:24 +1100

    defined the routes and components to respond to different URL requests

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,16 +1,46 @@
+import { Center } from "@chakra-ui/layout";
 import {
   Breadcrumb,
   BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
+  Stack,
+  Flex,
+  Heading,
+  Box,
 } from "@chakra-ui/react";
 
 export default function Nav({ links }) {
   return (
     <>
-      <Breadcrumb separator=" " boxSize={100} width={'full'} bg={"gray.700"}>
-        {links.map((link) => link)}
-      </Breadcrumb>
+      <Stack
+        direction={["column", "row"]}
+        spacing="24px"
+        align="center"
+        justify={{ base: "center", md: "space-between" }}
+        bg={"gray.700"}
+        boxSize={100}
+        width="100%"
+      >
+        <Center bg="gray.900" h="50px">
+          <Heading
+            fontSize={"2xl"}
+            fontFamily={"body"}
+            fontWeight={500}
+            color=" rgb(217, 185, 121)"
+          >
+            Shimna Puthanayil
+          </Heading>
+        </Center>
+
+        <Box bg="green.400">
+          <Breadcrumb separator=" " bg={"gray.700"} display="flex" px={50}>
+            {links.map((link) => (
+              <BreadcrumbItem color=" rgb(217, 185, 121)">
+                {link}
+              </BreadcrumbItem>
+            ))}
+          </Breadcrumb>
+        </Box>
+      </Stack>
     </>
   );
 }
