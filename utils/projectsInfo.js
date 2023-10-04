a3b5961dd79e3769b5f2b9229a2c7743ec9a4608
shimna <chimmu.p@gmail.com>
2023-10-04 21:02:25 +1100

    added a file projectInfo which returns project details

---
+++ utils/projectsInfo.js
@@ -0,0 +1,45 @@
+// import axios from 'axios';
+
+const getProjects = () => {
+  const projects = [
+    {
+      name: "RecipeBox",
+      url: "images/rb.jpeg",
+      deploymentLink:
+        "https://powerful-sierra-12005-787af3607f3d.herokuapp.com/",
+      description:
+        "RecipeBox is a user-friendly platform designed to bridge the gap between food enthusiasts and professional chefs. This project followed the MVC paradigm in its architectural structure",
+      gitHubLink: "https://github.com/J-D-garwood/recipe-forum",
+    },
+    {
+      name: "JustMovie",
+      url: "images/jm.jpeg",
+      deploymentLink: "https://shimna-puthanayil.github.io/movie-world/",
+      description:
+        "JustMovie is a movie website displaying a range of movies in conjunction with their ratings and reviews, enabling users to search for movies by title or through a series of categories including genre. ",
+      gitHubLink: "https://github.com/sakshiarora04/just-movie",
+    },
+    {
+      name: "TechBlog",
+      url: "images/tb.jpeg",
+      deploymentLink:
+        "https://murmuring-shelf-04632-c5a4a7d4538b.herokuapp.com/",
+      description:
+        "The project is a CMS-style blog site where developers can publish their blog posts and comment on other developers’ posts as well. They can also update or delete their posts. ",
+      gitHubLink: "https://github.com/shimna-puthanayil/tech-diary-m-v-c",
+    },
+    {
+      name: "CodeQuiz",
+      url: "images/quiz.jpeg",
+      deploymentLink: "https://shimna-puthanayil.github.io/code-quiz/",
+      description:
+        "The project helps to build a timed coding quiz that stores high scores.",
+      gitHubLink: "https://github.com/shimna-puthanayil/code-quiz",
+    },
+  ];
+  return projects;
+};
+
+export default {
+  getProjects,
+};
