86204bf4f2a0a32ed22f755f25502040aa36bb84
shimna <chimmu.p@gmail.com>
2023-10-07 00:00:28 +1100

    added the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,29 @@
 import { HStack, Link } from "@chakra-ui/react";
-import { FaGithub } from "react-icons/fa6";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa6";
+
 const gitHubLink = "https://github.com/shimna-puthanayil";
+const linkedInLink = "https://www.linkedin.com/in/shimna-puthanayil-766727173/";
+const twitterLink = "https://twitter.com/Shimna_Anoop";
 export default function Footer() {
   // The Footer component will render the links to github, linkedIn and Twitter
   return (
     <>
       <HStack
         display="flex"
-        alignItems="center"
+        // alignItems="center"
         justifyContent="center"
         height={20}
         bg={"gray.900"}
       >
-        <Link href={gitHubLink} isExternal>
-          <FaGithub size={"20px"} color={"#a5b5bc"} />
-        </Link>{" "}
+        <Link href={gitHubLink} ml={2} isExternal>
+          <FaGithub size={"20px"} color={"rgb(217, 185, 121)"} />
+        </Link>
+        <Link href={linkedInLink} ml={2} isExternal>
+          <FaLinkedin size={"20px"} color={"rgb(217, 185, 121)"} />
+        </Link>
+        <Link href={twitterLink} ml={2} isExternal>
+          <FaTwitter size={"20px"} color={"rgb(217, 185, 121)"} />
+        </Link>
       </HStack>
     </>
   );
